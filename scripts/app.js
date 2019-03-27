c584d7fcb194a8562878d10ff55ccb9460909b08
bakerbran89 <bakerbran89@gmail.com>
2019-03-27 12:03:16 -0700

    updated CDNs and content

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 //create module to be referenced in html
 //include custom module, [angular module: ngRoute]
-var appApp = angular.module('appApp', ['ngRoute']);
 
+
+var appApp = angular.module('appApp', ['ngRoute']);
 //configure routes to specified template and controller
 appApp.config(function($routeProvider){
     $routeProvider
@@ -30,23 +31,24 @@ appApp.config(function($routeProvider){
         });
 });
 
+
 //controllers for each page associated with above routes.  just contains a message for now
 appApp.controller('mainController', function($scope){
     $scope.message = 'Welcome to my projects website.  This site is currently under construction.  I will be updating this website with various learning tutorials.';
 });
 
 appApp.controller('basicController', function($scope){
-    $scope.message = 'My name is Brandon.  I like to camp, star gaze, play video games, and learn about computers.  I am a soon to be graduate in the exciting field of cybersecurity.  I will be working on this website as I transition from student to working professional.  I have learned a lot of important information through my cybersecurity coursework and would like to share my knowledge with others.  I look forward to building this website and hopefully inspiring others to learn something new.';
+    $scope.message = 'My name is Brandon.  I like to learn about computers.  I am a soon to be graduate in the exciting field of cybersecurity.  I will be working on this website as I transition from student to working professional.  I have learned a lot of important information through my cybersecurity coursework and would like to share my knowledge with others.  I look forward to building this website and hopefully inspiring others to learn something new.';
 });
 
 appApp.controller('portfolioController', function($scope){
-    $scope.message = 'All I have at the moment are learning projects and this website.  You can check them out on GitHub, u/n bakerbran89.';
+    $scope.message = 'This will be updated soon.  I might make it a resume page until I have some projects that I have designed myself.  Until then, assume that I am old and in the way.';
 });
 
 appApp.controller('projectsController', function($scope){
-    $scope.message = 'My current projects include developing successful personal/interpersonal traits, completing formal training in cybersecurity, and playing around with WebDev UI/UX.';
+    $scope.message = 'My current projects are based around learning.  I am trying to build a foundation in malware analysis, penetration testing, and apparently web development.  I will be adding tutorials to this section.';
 });
 
 appApp.controller('errorController', function($scope){
-    $scope.message = 'This page cannot be found.';
+    $scope.message = 'Page not found.  Either something is broken, or you are not using the nav-bar.';
 });
